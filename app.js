a7daa9830978e3b7ad342c8d24868ae5ccc55f80
baranncelik <121351869+baranncelik@users.noreply.github.com>
2025-07-24 23:41:06 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const userRoutes = require("./routers/user");
+const path = require("path");
+
+app.use(userRoutes);
+app.set("views", path.join(__dirname,"views"));
+app.set("view engine", "ejs"); 
+app.set(express.static(path.join("public")));
+
+
+app.use((req,res) =>{
+    res.status(404);
+    res.send("Aradığınız sayfa silinmiş ya da yanlış adres tıklamış olabilirsiniz.");
+
+})
+
+app.listen(3000);
